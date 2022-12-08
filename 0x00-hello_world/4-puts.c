3540699303d4bda7ccf5fa60ba18a99efc67e07c
nyarwayahugues <hunyagu23@gmail.com>
2022-12-08 12:41:07 -0800

    004

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
-/**
- * main - main block
- * return: 0
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+ #include <stdio.h>
 
-} 
+ /**
+ * main - Entry point
+ *
+ * return: always 0 (success)
+ */
+
+ int main(void)
+ {
+
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
+
+ }
+
